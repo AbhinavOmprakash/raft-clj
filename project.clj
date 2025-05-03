1bf8a4ab7228dcd0909be21fa1d91d6367748545
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2025-05-03 11:18:53 +0300

    Add marginalia and eftest

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/AbhinavOmprakash/raft-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-eftest "0.6.0"]]
+  :plugins [[lein-eftest "0.6.0"]
+            [lein-marginalia "0.9.2"]]
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns raft-clj.core})
