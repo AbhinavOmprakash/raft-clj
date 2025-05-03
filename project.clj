d4dcad2957326463f52393f868c0159e9c64f766
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2025-05-03 08:56:35 +0300

    Add lein eftest

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject org.clojars.abhinav/raft-clj "0.1.0-SNAPSHOT"
   :description "A raft under construction."
-  :url ""
+  :url "https://github.com/AbhinavOmprakash/raft-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-eftest "0.6.0"]]
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns raft-clj.core})
